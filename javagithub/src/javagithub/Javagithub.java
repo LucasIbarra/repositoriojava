9255d25dff6aea838b174f4bac57dcc7bdd4e85b
Lucas Ibarra <lucas05071995@gmail.com>
2023-03-23 18:22:23 -0300

    agregue la clase a la rama lucas

--- javagithub/src/javagithub/Javagithub.java
+++ javagithub/src/javagithub/Javagithub.java
@@ -10,12 +10,12 @@ package javagithub;
  * @author Lucas
  */
 public class Javagithub {
-
+    private String gato;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
